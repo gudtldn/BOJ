3e052c2ab16375364fece88b9660a0281fd7c6ff
gudtldn <gudtldn1234@gmail.com>
2024-05-30 02:20:50 +0900

    C++파일 추가시, 기본 템플릿 작성 코드 추가

--- make_folder.py
+++ make_folder.py
@@ -25,6 +25,26 @@ class Languages(Enum):
         return [*cls]
 
 
+def question_number() -> str:
+    while True:
+        n = input("문제 번호를 입력해 주세요: ")
+        if n.isnumeric():
+            return n
+
+        print("다시 입력해 주세요.\n")
+
+
+def selected_langs() -> list[Languages]:
+    print(*[f"{n}. {lang}" for n, lang in [(l.value, l.name) for l in Languages.all()]], sep="\n")
+    while True:
+        lang = list(input("언어를 선택해 주세요: ").replace(" ", ""))
+
+        if all(l.isnumeric() for l in lang):
+            return [l for l in Languages.all() if l.value in map(int, lang)]
+
+        print("다시 입력해 주세요.\n")
+
+
 def main():
     n = question_number()
     print()
@@ -49,32 +69,38 @@ def main():
                     system(f"cargo new boj_{n} --vcs none")
 
                 case Languages.Cpp:
-                    open(f"./boj_{n}.cpp", "w", encoding="utf-8").close()
+                    with open(f"./boj_{n}.cpp", "w", encoding="utf-8") as cpp,    \
+                         open("./run.bat", "w", encoding="utf-8") as run_bat,     \
+                         open("./debug.bat", "w", encoding="utf-8") as debug_bat, \
+                         open("./debug.gdb", "w", encoding="utf-8") as debug_gdb:
+                        cpp.write(
+                            "#include <iostream>\n\n"
+                            "#define dbg$(x) (printf(\"[%s:%d] %s = %d\\n\", __FILE__, __LINE__, #x, (x)), x)\n\n"
+                            "using namespace std;\n\n"
+                            "int main() {\n"
+                            "    ios::sync_with_stdio(false);\n"
+                            "    cin.tie(nullptr);\n"
+                            "    cout.tie(nullptr);\n\n\n\n"
+                            "    return 0;\n"
+                            "}\n"
+                        )
+                        run_bat.write(
+                            "@echo off\n"
+                            f"g++ -std=c++20 -o boj_{n} boj_{n}.cpp\n"
+                            f"boj_{n}.exe\n"
+                        )
+                        debug_gdb.write("r\nq\n")
+                        debug_bat.write(
+                            "@echo off\n"
+                            f"g++ -std=c++20 -g -o boj_{n} boj_{n}.cpp\n"
+                            f"gdb -q -x debug.gdb boj_{n}.exe\n"
+                        )
 
                 case Languages.Kotlin:
                     open(f"./boj_{n}.kt", "w", encoding="utf-8").close()
 
-    startfile(f"{getcwd()}\\{n}")
-
-
-def question_number() -> str:
-    while True:
-        n = input("문제 번호를 입력해 주세요: ")
-        if n.isnumeric():
-            return n
-
-        print("다시 입력해 주세요.\n")
-
-
-def selected_langs() -> list[Languages]:
-    print(*[f"{n}. {lang}" for n, lang in [(l.value, l.name) for l in Languages.all()]], sep="\n")
-    while True:
-        lang = list(input("언어를 선택해 주세요: ").replace(" ", ""))
-
-        if all(l.isnumeric() for l in lang):
-            return [l for l in Languages.all() if l.value in map(int, lang)]
-
-        print("다시 입력해 주세요.\n")
+    # startfile(f"{getcwd()}\\{n}")
+    system(f"code {getcwd()}\\{n}")
 
 
 if __name__ == "__main__":
