9cd6809e261fa6c9ec8b8ba8a9f93c6f72da1d9d
gudtldn <gudtldn1234@gmail.com>
2024-08-24 18:40:14 +0900

    Refactor make_folder.py for better code readability and maintainability

--- make_folder.py
+++ make_folder.py
@@ -126,32 +126,36 @@ def main():
                 case Languages.Rust:
                     system(f"cargo new boj_{n} --vcs none")
                     with open(f"./boj_{n}/src/main.rs", "w", encoding="utf-8") as rs:
-                        rs.write(
-                            f"// https://www.acmicpc.net/problem/{n}\n\n"
-                            "fn solution(stdin: &str) -> String {\n"
-                            "    let mut tokens = stdin.split('\\n');\n"
-                            "    let mut next = || tokens.next().unwrap();\n"
-                            "    let mut buffer = Vec::<String>::new();\n\n\n\n"
-                            "    buffer.join(\"\\n\")\n"
-                            "}\n\n"
-                            "#[cfg(test)]\n"
-                            "mod tests {\n"
-                            "    use super::*;\n\n"
-                            "    #[test]\n"
-                            "    fn test_solution1() {\n"
-                            "        assert_eq!(solution(/* input */), /* expected */);\n"
-                            "    }\n"
-                            "    /* add more tests */\n"
-                            "}\n\n"
-                            "fn main() {\n"
-                            "    use std::io::*;\n"
-                            "    println!(\"{}\", solution(&read_to_string(stdin()).unwrap()));\n"
-                            "    STDOUT.with(|refcell| std::io::Write::flush(&mut *refcell.borrow_mut()).unwrap());\n"
-                            "}\n\n"
-                            "thread_local! {\n"
-                            "    static STDOUT: std::cell::RefCell<std::io::BufWriter<std::io::StdoutLock<'static>>> = std::cell::RefCell::new(std::io::BufWriter::with_capacity(1 << 17, std::io::stdout().lock()));\n"
-                            "}\n"
-                        )
+                        rs.write(textwrap.dedent(f"""
+                            // https://www.acmicpc.net/problem/{n}
+                            fn solution(stdin: &str) -> String {{
+                                let mut tokens = stdin.split('\\n');
+                                let mut next = || tokens.next().unwrap();
+                                let mut buffer = Vec::<String>::new();
+                                buffer.join("\\n")
+                            }}
+
+                            #[cfg(test)]
+                            mod tests {{
+                                use super::*;
+
+                                #[test]
+                                fn test_solution1() {{
+                                    assert_eq!(solution(/* input */), /* expected */);
+                                }}
+                            }}
+
+                            fn main() {{
+                                use std::io::*;
+                                println!("{{}}", solution(&read_to_string(stdin()).unwrap()));
+                                STDOUT.with(|refcell| std::io::Write::flush(&mut *refcell.borrow_mut()).unwrap());
+                            }}
+
+                            thread_local! {{
+                                static STDOUT: std::cell::RefCell<std::io::BufWriter<std::io::StdoutLock<'static>>> =
+                                    std::cell::RefCell::new(std::io::BufWriter::with_capacity(1 << 17, std::io::stdout().lock()));
+                            }}
+                        """).lstrip())
 
                 case Languages.Cpp:
                     with (
@@ -160,50 +164,52 @@ def main():
                         open("./debug.bat", "w", encoding="utf-8") as debug_bat,
                         open("./debug.gdb", "w", encoding="utf-8") as debug_gdb
                     ):
-                        cpp.write(
-                            f"// https://www.acmicpc.net/problem/{n}\n\n"
-                            "#include <iostream>\n\n"
-                            "#define dbg$(x) (printf(\"[%s:%d] %s = %d\\n\", __FILE__, __LINE__, #x, (x)), x)\n\n"
-                            "using namespace std;\n\n"
-                            "int main() {\n"
-                            "    ios::sync_with_stdio(false);\n"
-                            "    cin.tie(nullptr);\n"
-                            "    cout.tie(nullptr);\n\n\n\n"
-                            "    return 0;\n"
-                            "}\n"
-                        )
-                        run_bat.write(
-                            "@echo off\n"
-                            f"g++ -std=c++20 -o boj_{n} boj_{n}.cpp\n"
-                            "IF %ERRORLEVEL% NEQ 0 EXIT\n"
-                            f"boj_{n}.exe\n"
-                        )
+                        cpp.write(textwrap.dedent(f"""
+                            // https://www.acmicpc.net/problem/{n}
+                            #include <iostream>
+                            #define FASTIO cin.tie(nullptr); cout.tie(nullptr); ios::sync_with_stdio(false);
+
+                            int main() {{
+                                using namespace std;
+                                FASTIO
+
+                                // code here
+
+                                return 0;
+                            }}
+                        """).lstrip())
+                        run_bat.write(textwrap.dedent(f"""
+                            @echo off
+                            g++ -std=c++20 -o boj_{n} boj_{n}.cpp
+                            IF %ERRORLEVEL% NEQ 0 EXIT
+                            boj_{n}.exe
+                        """).lstrip())
                         debug_gdb.write("r\nq\n")
-                        debug_bat.write(
-                            "@echo off\n"
-                            f"g++ -std=c++20 -g -o boj_{n} boj_{n}.cpp\n"
-                            "IF %ERRORLEVEL% NEQ 0 EXIT\n"
-                            f"gdb -q -x debug.gdb boj_{n}.exe\n"
-                        )
+                        debug_bat.write(textwrap.dedent(f"""
+                            @echo off
+                            g++ -std=c++20 -g -o boj_{n} boj_{n}.cpp
+                            IF %ERRORLEVEL% NEQ 0 EXIT
+                            gdb -q -x debug.gdb boj_{n}.exe
+                        """).lstrip())
 
                 case Languages.Kotlin:
                     with (
                         open(f"./boj_{n}.kt", "w", encoding="utf-8") as kt,
                         open("./run.bat", "w", encoding="utf-8") as run_bat,
                     ):
-                        kt.write(
-                            f"// https://www.acmicpc.net/problem/{n}\n\n"
-                            "fun main() {\n\n"
-                            "}\n"
-                        )
-                        run_bat.write(
-                            "@echo off\n"
-                            f"kotlinc boj_{n}.kt -include-runtime -d boj_{n}.jar\n"
-                            "IF %ERRORLEVEL% NEQ 0 EXIT\n"
-                            f"java -jar boj_{n}.jar\n"
-                        )
+                        kt.write(textwrap.dedent(f"""
+                            // https://www.acmicpc.net/problem/{n}
+                            fun main() {{
+                                // code here
+                            }}
+                        """).lstrip())
+                        run_bat.write(textwrap.dedent(f"""
+                            @echo off
+                            kotlinc boj_{n}.kt -include-runtime -d boj_{n}.jar
+                            IF %ERRORLEVEL% NEQ 0 EXIT
+                            java -jar boj_{n}.jar
+                        """).lstrip())
 
-    # startfile(f"{getcwd()}\\{n}")
     system(f"code {getcwd()}\\{n}")
 
 if __name__ == "__main__":
