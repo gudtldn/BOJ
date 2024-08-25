4984865f14078cb01282bdf64a0e1bfef9a75114
gudtldn <gudtldn1234@gmail.com>
2024-08-25 15:47:13 +0900

    chore: Refactor make_folder.py for better code readability and maintainability

--- make_folder.py
+++ make_folder.py
@@ -137,61 +137,64 @@ def main():
                                 todo!("solve here");
                             }}
 
-                        #[cfg(test)]
-                        mod tests {{
-                            use super::*;
-                            fn capture_output(f: impl FnOnce()) -> String {{
-                                STDOUT.with(|stdout| {{
-                                    stdout.borrow_mut().clear();
-                                }});
-                                f();
-                                STDOUT.with(|stdout| {{
-                                    String::from_utf8(stdout.borrow().to_vec()).unwrap()
-                                }})
+                            #[cfg(test)]
+                            mod tests {{
+                                use super::*;
+                                fn capture_output(f: impl FnOnce()) -> String {{
+                                    STDOUT.with(|stdout| {{
+                                        stdout.borrow_mut().clear();
+                                    }});
+                                    f();
+                                    STDOUT.with(|stdout| {{
+                                        String::from_utf8(stdout.borrow().to_vec())
+                                            .unwrap()
+                                            .trim_end()
+                                            .to_string()
+                                    }})
+                                }}
+
+                                #[test]
+                                fn test_solution1() {{
+                                    assert_eq!(capture_output(|| solution(...)), ...);
+                                }}
                             }}
 
-                            #[test]
-                            fn test_solution1() {{
-                                assert_eq!(capture_output(|| solution(...)), ...);
+                            fn main() {{
+                                use std::io::*;
+                                solution(&read_to_string(stdin()).unwrap());
+                                STDOUT.with(|refcell| std::io::Write::flush(&mut *refcell.borrow_mut()).unwrap());
                             }}
-                        }}
 
-                        fn main() {{
-                            use std::io::*;
-                            solution(&read_to_string(stdin()).unwrap());
-                            STDOUT.with(|refcell| std::io::Write::flush(&mut *refcell.borrow_mut()).unwrap());
-                        }}
+                            #[cfg(test)]
+                            thread_local! {{
+                                static STDOUT: std::cell::RefCell<Vec<u8>> = std::cell::RefCell::new(Vec::new());
+                            }}
 
-                        #[cfg(test)]
-                        thread_local! {{
-                            static STDOUT: std::cell::RefCell<Vec<u8>> = std::cell::RefCell::new(Vec::new());
-                        }}
+                            #[cfg(not(test))]
+                            thread_local! {{
+                                static STDOUT: std::cell::RefCell<std::io::BufWriter<std::io::StdoutLock<'static>>> =
+                                std::cell::RefCell::new(std::io::BufWriter::with_capacity(1 << 17, std::io::stdout().lock()));
+                            }}
 
-                        #[cfg(not(test))]
-                        thread_local! {{
-                            static STDOUT: std::cell::RefCell<std::io::BufWriter<std::io::StdoutLock<'static>>> =
-                            std::cell::RefCell::new(std::io::BufWriter::with_capacity(1 << 17, std::io::stdout().lock()));
-                        }}
+                            #[macro_export]
+                            macro_rules! println {{
+                                ($($t:tt)*) => {{
+                                    STDOUT.with(|refcell| {{
+                                        use std::io::*;
+                                        writeln!(refcell.borrow_mut(), $($t)*).unwrap();
+                                    }});
+                                }};
+                            }}
 
-                        #[macro_export]
-                        macro_rules! println {{
-                            ($($t:tt)*) => {{
-                                STDOUT.with(|refcell| {{
-                                    use std::io::*;
-                                    writeln!(refcell.borrow_mut(), $($t)*).unwrap();
-                                }});
-                            }};
-                        }}
-
-                        #[macro_export]
-                        macro_rules! print {{
-                            ($($t:tt)*) => {{
-                                STDOUT.with(|refcell| {{
-                                    use std::io::*;
-                                    write!(refcell.borrow_mut(), $($t)*).unwrap();
-                                }});
-                            }};
-                        }}
+                            #[macro_export]
+                            macro_rules! print {{
+                                ($($t:tt)*) => {{
+                                    STDOUT.with(|refcell| {{
+                                        use std::io::*;
+                                        write!(refcell.borrow_mut(), $($t)*).unwrap();
+                                    }});
+                                }};
+                            }}
                         """).lstrip())
 
                 case Languages.Cpp:
